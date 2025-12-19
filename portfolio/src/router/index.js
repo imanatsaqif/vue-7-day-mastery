c37ba28ba286cd98e01428e57829e084773ac22f
Imana Tsaqif <iimtsaqif@gmail.com>
2025-12-19 11:08:23 +0700

    add Vue Router with layout-based routing

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainLayout from '../layouts/MainLayout.vue'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Projects from '../views/Projects.vue'
+import ProjectDetail from '../views/ProjectDetail.vue'
+import Contact from '../views/Contact.vue'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      { path: '', component: Home },
+      { path: 'about', component: About },
+      { path: 'projects', component: Projects },
+      { path: 'projects/:id', component: ProjectDetail },
+      { path: 'contact', component: Contact }
+    ]
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
