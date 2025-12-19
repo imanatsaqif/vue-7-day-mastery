e2e7ca93e785b8b018ccd9094f49706d9cf83aeb
Imana Tsaqif <iimtsaqif@gmail.com>
2025-12-19 17:20:59 +0700

    adding technology on project details

--- portfolio/src/views/ProjectDetail.vue
+++ portfolio/src/views/ProjectDetail.vue
@@ -19,6 +19,12 @@
   <section v-if="project" class="mx-auto">
     <h1>{{ project.title }}</h1>
     <p class="mt-4">{{ project.description }}</p>
+    <div class="mt-6">
+      <h2 class="text-xl font-semibold mb-2">Technologies Used:</h2>
+      <ul class="list-disc list-inside">
+        <li v-for="tech in project.tech" :key="tech">{{ tech }}</li>
+      </ul>
+    </div>
   </section>
   <section v-else class="text-center">
     <h1 class="text-2xl font-semibold">Project Not Found</h1>
